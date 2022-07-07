88f75d3d4061734e65bc9456664d9bfb9ffcc9c2
AlekseyDemydov <96746949+AlekseyDemydov@users.noreply.github.com>
2022-07-07 15:38:26 +0300

    up

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './css/styles.css';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import debounce from 'lodash.debounce';
+// import { fetchCountries } from './js/fetchCountries';
+import card from './part/countries-card.hbs';
+
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box');
+const listEl = document.querySelector('.country-list');
+const infoEl = document.querySelector('.country-info');
+
+// inputEl.addEventListener('input', fetchCountries());
+fetchCountries('peru').then(renderCard);
+
+function fetchCountries(name) {
+  return fetch('https://restcountries.com/v3.1/name/`${name}`').then(
+    response => {
+      return response.json();
+    }
+  );
+}
+
+function renderCard(countries) {
+  const markup = card(countries);
+
+  console.log(markup);
+  listEl.innerHTML = markup;
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,14 @@
+const urlLink = 'https://restcountries.com/v3.1/name';
+
+const fetchCountries = name => {
+  return fetch(
+    `${urlLink}/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (response.status === 404) {
+      return Promise.reject(new Error());
+    }
+    return response.json();
+  });
+};
+
+export { fetchCountries };
